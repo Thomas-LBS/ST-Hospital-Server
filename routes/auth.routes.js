0e458d5494712700669754d0f2628128a32729f8
Sunitha-Arockia-Dass <135265573+Sunitha-Arockia-Dass@users.noreply.github.com>
2023-11-20 21:26:33 +0100

    patient update changed inside auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -134,5 +134,77 @@ router.get("/verify", isAuthenticated, (req, res, next) => {
   // Send back the token payload object containing the user data
   res.status(200).json(req.payload);
 });
+router.put("/update/:id", (req, res, next) => {
+  const userId = req.params.id;
+  const updatedUserInfo=req.body
+  let { email, password, name,role ,firstname,lastname,patientDetails,newPassword} = updatedUserInfo;
+  if (email === "" || password === "" || name === "" || firstname === "" || lastname === ""  ) {
+    res.status(400).json({ message: "Provide email, password , name , firstname and lastname" });
+    return;
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: "Provide a valid email address." });
+    return;
+  }
+  // // This regular expression checks password for special characters and minimum length
+  // const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  // if (!passwordRegex.test(password)) {
+  //   res.status(400).json({
+  //     message:
+  //       "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+  //   });
+  //   return;
+  // }
+
+  if (newPassword === "") {
+    newPassword = password;
+  }
+  User.findById(userId)
+    .then((foundUser) => {
+      // console.log("foundUser", foundUser);
+      // If the user with the same email already exists, send an error response
+      if (!foundUser) {
+        res.status(400).json({ message: "User Not Found." });
+        return;
+      }
+      const isSamePassword = bcrypt.compareSync(password, foundUser.password);
+
+    if (!isSamePassword) {
+      res.status(400).json({ errorMessage: "Wrong credentials." });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(newPassword, salt);
+
+        // bcrypt
+        //   .genSalt(saltRounds)
+        //   .then((salt) => bcrypt.hashSync(newPassword, salt))
+        //   .then((hashedPassword) => {
+            // Create a user and save it in the database
+            return User.findByIdAndUpdate(
+              userId,
+             { email, password:hashedPassword, username:name,role ,firstname,lastname,patientDetails}
+              ,
+              { new: true }
+            ).then((updatedUser) => {
+              if (!updatedUser) {
+                res.status(500).json({ message: "Error creating user." });
+                return;
+              }
+              const { email, name, _id, role } = updatedUser;
+              const user = { email, name, _id, role };
+              console.log(user);
+              // Send a json response containing the user object
+              res.status(201).json({ user: user });
+              
+            });
+          })
+          .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
+  
+      
+    // })
+});
+
 
 module.exports = router;
